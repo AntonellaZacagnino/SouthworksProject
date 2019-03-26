d1d9750953d73375ba34b479617b033004b54d61
Unknown <antonellazacagnino@gmail.com>
2019-03-26 16:48:46 -0300

    Initial commit

---
+++ testproject/Data/testprojectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using testproject;
+
+namespace testproject.Models
+{
+    public class testprojectContext : DbContext
+    {
+        public testprojectContext (DbContextOptions<testprojectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<testproject.Patient> Patient { get; set; }
+    }
+}
---
+++ testproject/Migrations/20190325143301_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace testproject.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patient");
+        }
+    }
+}
